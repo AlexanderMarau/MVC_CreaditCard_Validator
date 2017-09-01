21e8542bbdde0f60633e0386966e2c05dc81f062
Carlos Roberto Rojas <crojas@ecoinfo.com.co>
2017-09-01 09:59:50 -0500

    Agregar archivos de proyecto.

---
+++ TarjetaCredito/Controllers/TarjetaCreditoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TarjetaCredito.Models;
+
+namespace TarjetaCredito.Controllers
+{
+    public class TarjetaCreditoController : Controller
+    {
+        // GET: TarjetaCredito
+        public ActionResult Index()
+        {
+            return HttpNotFound();
+        }
+
+
+        
+        public ActionResult Validar()
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                string numeroTarjeta = Request.Form.GetValues("numero")[0];
+                TarjetaCredito.Models.TarjetaCredito nuevaTarjeta = 
+                    new TarjetaCredito.Models.TarjetaCredito(numeroTarjeta);
+                if (!nuevaTarjeta.validar())
+                    return View(nuevaTarjeta);
+                else
+                    return View("ValidacionExitosa",nuevaTarjeta);
+            }
+            else
+                return View();
+        }
+    }
+}
---
+++ TarjetaCredito/Models/TarjetaCredito.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace TarjetaCredito.Models
+{
+    public class TarjetaCredito
+    {
+        public string numero { get; set; }
+
+        public TarjetaCredito(string numero)
+        {
+            this.numero = numero;
+        }
+
+        public bool validar()
+        {
+            if (!new Regex(@"^\d{16}$").IsMatch(numero))
+                return false;
+            else
+            {
+                bool esPar = true;
+                int suma = 0;
+                foreach (char caracter in numero)
+                {
+                    if (esPar)
+                    {
+                        var temp = 2 * int.Parse(caracter.ToString());
+                        temp = temp / 10 + temp % 10;
+                        suma += temp;
+                    }
+                    else
+                    {
+                        suma += int.Parse(caracter.ToString());
+                    }
+                    esPar = !esPar;
+                }
+                return suma % 10 == 0;
+            }
+        }
+
+        public static List<bool> validatTarjetas(List<TarjetaCredito> lista)
+        {
+            List<bool> listaResultados = new List<bool>();
+            foreach (var tarjeta in lista)
+            {
+                listaResultados.Add(tarjeta.validar());
+            }
+            return listaResultados;
+        }
+    }
+}
